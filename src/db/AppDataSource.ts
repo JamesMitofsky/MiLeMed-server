4943a0d2bd26fa912867f49628de31f5ec5c30f8
James Mitofsky <JamesMitofsky@gmail.com>
2024-09-28 12:16:48 +0200

    Add params for heroku

--- src/db/AppDataSource.ts
+++ src/db/AppDataSource.ts
@@ -3,12 +3,15 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
   name: 'default',
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DATABASE_URL || 'localhost',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
   database: 'milemed',
-  synchronize: true,
+  synchronize: process.env.NODE_ENV === 'development',
   logging: true,
+  ssl: {
+    rejectUnauthorized: false, // Necessary for Heroku
+  },
   entities: ['src/entity/*.*'],
 });
